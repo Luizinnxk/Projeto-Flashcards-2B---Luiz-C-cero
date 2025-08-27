6d734ba5191382384531424f3d43a5b6d28476ee
luizinnx <luiz.souza.silva12@escola.pr.gov.br>
2025-08-27 12:03:39 -0300

    Create perguntas.js

---
+++ perguntas.js
@@ -0,0 +1,61 @@
+// assets/perguntas.js
+// 10 flashcards sobre programação
+criaCartao(
+    'Programação',
+    'O que é JavaScript?',
+    'O JavaScript é uma linguagem de programação amplamente utilizada para desenvolvimento web.'
+);
+
+criaCartao(
+    'Programação',
+    'O que é HTML?',
+    'HTML é uma linguagem de marcação utilizada na construção de páginas na Web.'
+);
+
+criaCartao(
+    'Programação',
+    'O que é CSS?',
+    'CSS é uma linguagem de estilização utilizada para definir a apresentação de documentos HTML.'
+);
+
+criaCartao(
+    'Programação',
+    'O que é um algoritmo?',
+    'Um algoritmo é uma sequência finita de ações executáveis que visam obter uma solução para um determinado tipo de problema.'
+);
+
+criaCartao(
+    'Programação',
+    'O que é Git?',
+    'Git é um sistema de controle de versões distribuído, usado principalmente no desenvolvimento de software.'
+);
+
+criaCartao(
+    'Programação',
+    'O que é um framework?',
+    'Um framework é uma abstração que fornece funcionalidades genéricas que podem ser seletivamente alteradas por código adicional.'
+);
+
+criaCartao(
+    'Programação',
+    'O que é API?',
+    'API significa Interface de Programação de Aplicações, um conjunto de rotinas e padrões para acesso a um aplicativo ou plataforma.'
+);
+
+criaCartao(
+    'Programação',
+    'O que é React?',
+    'React é uma biblioteca JavaScript para criar interfaces de usuário, mantida pelo Facebook.'
+);
+
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'Python é uma linguagem de programação de alto nível, interpretada, de script, imperativa, orientada a objetos, funcional, de tipagem dinâmica e forte.'
+);
+
+criaCartao(
+    'Programação',
+    'O que é SQL?',
+    'SQL é uma linguagem de consulta estruturada usada para gerenciar e manipular bancos de dados relacionais.'
+);
